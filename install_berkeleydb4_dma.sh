3ccb5ce148bf456db665b1a63923729e0d3efdb4
Domimrc <dominik.marchsreiter@gmail.com>
2024-04-14 12:40:36 +0200

    added install_berkleydb4.8

---
+++ install_berkeleydb4_dma.sh
@@ -0,0 +1,7 @@
+wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+tar -xvf db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix
+mkdir -p build
+BDB_PREFIX=$(pwd)/build
+../dist/configure --disable-shared --enable-cxx --with-pic --prefix=$BDB_PREFIX
+sudo make install
